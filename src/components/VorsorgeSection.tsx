8e84fcc5809fed05973a3cbc495f0701f98ead63
unknown <deineemailadresse>
2025-12-24 16:28:29 +0100

    v3.11.93: Fine-tune to w-[560px] - getting closer

--- src/components/VorsorgeSection.tsx
+++ src/components/VorsorgeSection.tsx
@@ -53,7 +53,7 @@ export function VorsorgeSection({ searchTerm, setIsAddFixedDepositModalOpen, set
                                 <th className="px-2 py-3 sticky left-0 z-20 bg-card shadow-[5px_0_5px_-5px_rgba(0,0,0,0.1)] min-w-[200px]">Bank / Institut</th>
                                 <th className="px-2 py-3 min-w-[100px]">Konto-Typ</th>
                                 <th className="px-4 py-3 text-right whitespace-nowrap w-[80px]">Vermögen</th>
-                                <th className="px-4 py-3 w-[580px]">Fortschritt 2025</th>
+                                <th className="px-4 py-3 w-[560px]">Fortschritt 2025</th>
                                 <th className="px-1 py-3 text-center sticky right-0 bg-card z-10 w-[60px] shadow-[-5px_0_5px_-5px_rgba(0,0,0,0.1)]">Aktion</th>
                             </tr>
                         </thead>
